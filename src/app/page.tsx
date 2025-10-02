e0d6ea7be0a165539cb27f606f5eda7cd73d8137
MirjalilovS <MirjalilovS@users.noreply.github.com>
2025-10-02 11:54:34 +0100

    New Changes to CV and Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ const projects = [
     title: 'MNIST Digit Recognition Service',
     description: 'Deployed an interactive machine learning service with a Streamlit frontend and a Docker-containerised PostgreSQL database to serve a PyTorch CNN model.',
     link: 'n/a',
-  tags: ["Python", "PyTorch", "Streamlit", "Docker", "PostgreSQL", "Machine Learning", "CNN"]
+    tags: ["Python", "PyTorch", "Streamlit", "Docker", "PostgreSQL", "Machine Learning", "CNN"]
   },
   {
     id: 5,
@@ -49,20 +49,27 @@ const projects = [
 const experiences = [
   {
     id: 1,
+    role: "Software Developer Intern",
+    company: "Moneff - Bootstrapepd Fintech Startup",
+    description: "During this fintech internship, I gained comprehensive, hands-on experience across the full software lifecycle. I honed my backend development skills through coding, debugging, pair programming and basic database operations, while also developing a practical understanding of modern DevOps practices like CI/CD workflows, containerization, and system monitoring. Working within a fast-paced, agile, and cross-functional team was instrumental in sharpening my collaboration, communication, and problem-solving abilities. I had two mentors who allowed me to get stuck in from day one and greatly accelerated my development as a Software Engineer.",
+    dates: "Sep. 2025 – Dec. 2025",
+  },
+  {
+    id: 2,
     role: "Open Source Contributor",
     company: "DVC - MLOps Platform",
     description: "I engineered the --hide-workspace flag in Python with accompanying unit tests. I collaborated with the project maintainers through Pull Request (#10798) to achieve a successful merge. I also authored the official documentation on Pull Request (#5427). I demonstrated deep engagement by identifying and flagging three additional undocmented flags under exp show.",
     dates: "Jul. 2025 – Aug. 2025",
   },
   {
-    id: 2,
+    id: 3,
     role: "Automation Engineering Intern",
     company: "Warwick Manufacturing Group",
     description: "I slashed a 3-week manual workflow to 3 minutes by developing an automated VBA macro to aid in the STPA process. I improved analysis consistency across Safety Analysts and ported the VBA process into a bare-bones Python backend framework for the companies software engineers to use when they begin developing their own inhouse tool.",
     dates: "Jul. 2024 – Sep. 2024",
   },
   {
-    id: 3,
+    id: 4,
     role: "Backend Engineer Intern",
     company: "Uzum - Tech Unicorn",
     description: "I desgined and developed an automated data pipeline to extract data from a MariaDB database and process over 5,000 client locations. This pipeline was integrated into the company's main KPI dashboard to improve operational planning through a geospatial visualisation model. My work culminated in developing an automated tracking algorithm that aided the companies lawyers in halving the debt recovery timeline for over a 1,000 cases.",
@@ -88,7 +95,7 @@ export default function Home() {
       </div>
 
       <p className="pt-4 text-sm text-foreground">
-        Hi there, I'm Samandar, a Mathematics graduate from the University of Warwick. I'm passionate about building scalable backend systems and full-stack applications that solve real-world problems. 
+        Hi there, I'm Samandar, a Mathematics graduate from the University of Warwick. I'm passionate about building scalable backend systems and full-stack applications that solve real-world problems.
       </p>
 
       <SocialLinks />
@@ -103,28 +110,28 @@ export default function Home() {
       </section>
 
       <section className="pt-8">
-  <h2 className="font-medium tracking-tight text-foreground">Projects</h2>
+        <h2 className="font-medium tracking-tight text-foreground">Projects</h2>
 
-  <p className="text-sm text-foreground/70">
-    Visit my{' '}
-    <Link
-      href="https://github.com/MirjalilovS?tab=repositories"
-      rel="noopener noreferrer"
-      target="_blank"
-      className="font-medium text-primary underline underline-offset-2 transition-all duration-150 ease-in-out hover:text-accent"
-    >
-      GitHub
-    </Link>{' '}
-    for all my projects.
-  </p>
+        <p className="text-sm text-foreground/70">
+          Visit my{' '}
+          <Link
+            href="https://github.com/MirjalilovS?tab=repositories"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="font-medium text-primary underline underline-offset-2 transition-all duration-150 ease-in-out hover:text-accent"
+          >
+            GitHub
+          </Link>{' '}
+          for all my projects.
+        </p>
 
-  <div className="pt-1.5">
-    {projects.map(project => (
-      <ProjectCard key={project.id} project={project} />
-    ))}
-  </div>
+        <div className="pt-1.5">
+          {projects.map(project => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
+        </div>
 
-</section>
+      </section>
 
       <Footer />
     </>
